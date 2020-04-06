8b21d4933f1c7e33e8cc2d2d89db7d359b567e72
mlanneponders <mlyonadi@gmail.com>
2020-04-05 21:02:38 -0300

    Cierro warnings

--- src/main/java/dominio/Operacion.java
+++ src/main/java/dominio/Operacion.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Operacion {
     private int precioFinal;
     private List<Item> items;
-    private Estado estado = Estado.ABIERTO;
+    private Estado estado ;
 
     public Operacion(int precioFinal, List<Item> items, Estado estado) {
         this.precioFinal = precioFinal;
@@ -19,12 +19,36 @@ public class Operacion {
         this.precioFinal = total;
         return total;
     }
-
     private int calcularPrecioTotal() {
-        int precioTotal = this.items.stream()
-                .mapToInt(item -> item.getPrecio())
+        return this.items.stream()
+                .mapToInt(Item::getPrecio)
                 .sum();
-        return precioTotal;
     }
+
+    public int getPrecioFinal() {
+        return precioFinal;
+    }
+
+    public void setPrecioFinal(int precioFinal) {
+        this.precioFinal = precioFinal;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+
 
 }
